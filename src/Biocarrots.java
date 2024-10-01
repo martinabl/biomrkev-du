cb057dfcbbac5ff283bdbc03226c1966096baaf6
martinabl <matinka271664@gmail.com>
2024-10-01 19:40:40 +0200

    Komentář na zkoušku
    Přidání komentáře pro vyzkoušní publikování změn

---
+++ src/Biocarrots.java
@@ -0,0 +1,20 @@
+public class Biocarrots {
+    private static String name;
+    private String address;
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Biocarrots.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/Sellers.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Sellers {
+    private LocalDate birthday;
+    private int contracts;
+    private int sale;
+    private String punch;
+    private String SPZ;
+    private Double consumption;
+    private Biocarrots biocarrots;
+
+}
